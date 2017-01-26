746cd63f2cc302099414d6f44ac4396608ede1ce
Chandu Tennety <chandu.tennety@gmail.com>
2017-01-26 14:20:03 -0500

    Make hiding the poster configurable

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -2149,6 +2149,8 @@ function isAfter(sourceNode, targetNode) {
 	mejs.MepDefaults = {
 		// url to poster (to fix iOS 3.x)
 		poster: '',
+		// Whether to hide the poster on play (useful for audio files)
+		hidePosterOnPlay: true,
 		// When the video is ended, we can show the poster.
 		showPosterWhenEnded: false,
 		// default if the <video width> is not specified
@@ -3277,7 +3279,7 @@ function isAfter(sourceNode, targetNode) {
 			}
 
 			media.addEventListener('play',function() {
-				poster.hide();
+				if (player.options.hidePosterOnPlay) { poster.hide(); }
 			}, false);
 
 			if(player.options.showPosterWhenEnded && player.options.autoRewind){

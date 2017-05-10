066a2faedcc7600eeb6a3a4f0acfc5b9220e3f6d
Chandu Tennety <ctennety@instructure.com>
2017-05-10 11:30:01 -0400

    Back-port track sanitizing to prevent XSS
    https://github.com/mediaelement/mediaelement/commit/fd88ce0e2fab8e02f8ab8e00a0b2bfc5769966cf

--- src/js/mep-feature-tracks.js
+++ src/js/mep-feature-tracks.js
@@ -360,14 +360,47 @@
 
 			var
 				t = this,
+				track = t.selectedTrack,
 				i,
-				track = t.selectedTrack;
+				sanitize = function (html) {
+
+					var div = document.createElement('div');
+
+					div.innerHTML = html;
+
+					// Remove all `<script>` tags first
+					var scripts = div.getElementsByTagName('script');
+					var i = scripts.length;
+					while (i--) {
+						scripts[i].parentNode.removeChild(scripts[i]);
+					}
+
+					// Loop the elements and remove anything that contains value="javascript:" or an `on*` attribute
+					// (`onerror`, `onclick`, etc.)
+					var allElements = div.getElementsByTagName('*');
+					for (var i = 0, n = allElements.length; i < n; i++) {
+						var
+							attributesObj = allElements[i].attributes,
+							attributes = Array.prototype.slice.call(attributesObj)
+						;
+
+						for (var j = 0, total = attributes.length; j < total; j++) {
+							if (attributes[j].name.startsWith('on') || attributes[j].value.startsWith('javascript')) {
+								allElements[i].parentNode.removeChild(allElements[i]);
+							} else if (attributes[j].name === 'style') {
+								allElements[i].removeAttribute(attributes[j].name);
+							}
+						}
+
+					}
+					return div.innerHTML;
+				};
 
 			if (track !== null && track.isLoaded) {
 				for (i=0; i<track.entries.times.length; i++) {
 					if (t.media.currentTime >= track.entries.times[i].start && t.media.currentTime <= track.entries.times[i].stop) {
 						// Set the line before the timecode as a class so the cue can be targeted if needed
-						t.captionsText.html(track.entries.text[i]).attr('class', 'mejs-captions-text ' + (track.entries.times[i].identifier || ''));
+						t.captionsText.html(sanitize(track.entries.text[i])).attr('class', 'mejs-captions-text ' + (track.entries.times[i].identifier || ''));
 						t.captions.show().height(0);
 						return; // exit out if one is visible;
 					}
